6d0d74a29a96df9991c1f548e1cdd581bb1300b3
Your Name <safynazabdelraheem@gmail.com>
2024-08-28 13:57:42 +0300

    vue

---
+++ lab1 vue/lab1/books.js
@@ -0,0 +1,157 @@
+let books = [
+  
+  {
+    ISBN: '901234567',
+    NAME: 'The Book Thief',
+    CATEGORY: 'Historical Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780375842207',
+    NUMBER_OF_PAGES: 552,
+    AUTHOR: 'Markus Zusak',
+    PRICE: 15
+  },
+  {
+    ISBN: '112233445',
+    NAME: 'Pride and Prejudice',
+    CATEGORY: 'Classic Literature',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780141439518',
+    NUMBER_OF_PAGES: 50,
+    AUTHOR: 'Jane Austen',
+    PRICE: 12
+  },
+  {
+    ISBN: '223344556',
+    NAME: 'The Catcher in the Rye',
+    CATEGORY: 'Classic Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780316769174',
+    NUMBER_OF_PAGES: 224,
+    AUTHOR: 'J.D. Salinger',
+    PRICE: 13
+  },
+  {
+    ISBN: '334455667',
+    NAME: 'The Great Gatsby',
+    CATEGORY: 'Classic Literature',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780743273565',
+    NUMBER_OF_PAGES: 180,
+    AUTHOR: 'F. Scott Fitzgerald',
+    PRICE: 11
+  },
+  {
+    ISBN: '445566778',
+    NAME: '1984',
+    CATEGORY: 'Dystopian Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780451524935',
+    NUMBER_OF_PAGES: 328,
+    AUTHOR: 'George Orwell',
+    PRICE: 12
+  },
+  {
+    ISBN: '556677889',
+    NAME: 'The Help',
+    CATEGORY: 'Historical Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780425232200',
+    NUMBER_OF_PAGES: 544,
+    AUTHOR: 'Kathryn Stockett',
+    PRICE: 18
+  },
+  {
+    ISBN: '667788990',
+    NAME: 'The Handmaid\'s Tale',
+    CATEGORY: 'Dystopian Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780385490818',
+    NUMBER_OF_PAGES: 311,
+    AUTHOR: 'Margaret Atwood',
+    PRICE: 14
+  },
+  {
+    ISBN: '778899001',
+    NAME: 'The Lord of the Rings',
+    CATEGORY: 'Fantasy',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780618640157',
+    NUMBER_OF_PAGES: 1178,
+    AUTHOR: 'J.R.R. Tolkien',
+    PRICE: 25
+  },
+  {
+    ISBN: '889900112',
+    NAME: 'The Fault in Our Stars',
+    CATEGORY: 'Young Adult',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780525478812',
+    NUMBER_OF_PAGES: 313,
+    AUTHOR: 'John Green',
+    PRICE: 10
+  },
+  {
+    ISBN: '012345678',
+    NAME: 'The Alchemist',
+    CATEGORY: 'Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780061122415',
+    NUMBER_OF_PAGES: 197,
+    AUTHOR: 'Paulo Coelho',
+    PRICE: 20
+  },
+  {
+    ISBN: '234567890',
+    NAME: 'The Hunger Games',
+    CATEGORY: 'Young Adult',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780439023528',
+    NUMBER_OF_PAGES: 374,
+    AUTHOR: 'Suzanne Collins',
+    PRICE: 17
+  },
+  {
+    ISBN: '345678901',
+    NAME: 'The Da Vinci Code',
+    CATEGORY: 'Mystery',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780307474278',
+    NUMBER_OF_PAGES: 689,
+    AUTHOR: 'Dan Brown',
+    PRICE: 22
+  },
+  {
+    ISBN: '456789012',
+    NAME: 'Gone Girl',
+    CATEGORY: 'Thriller',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780307588371',
+    NUMBER_OF_PAGES: 419,
+    AUTHOR: 'Gillian Flynn',
+    PRICE: 19
+  },
+  {
+    ISBN: '567890123',
+    NAME: 'The Girl with the Dragon Tattoo',
+    CATEGORY: 'Crime',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780307269751',
+    NUMBER_OF_PAGES: 672,
+    AUTHOR: 'Stieg Larsson',
+    PRICE: 21
+  },
+  {
+    ISBN: '678901234',
+    NAME: 'The Martian',
+    CATEGORY: 'Science Fiction',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780553418026',
+    NUMBER_OF_PAGES: 369,
+    AUTHOR: 'Andy Weir',
+    PRICE: 16
+  },
+  {
+    ISBN: '789012345',
+    NAME: 'The Night Circus',
+    CATEGORY: 'Fantasy',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780307744432',
+    NUMBER_OF_PAGES: 512,
+    AUTHOR: 'Erin Morgenstern',
+    PRICE: 18
+  },
+  {
+    ISBN: '890123456',
+    NAME: 'The Road',
+    CATEGORY: 'Post-Apocalyptic',
+    IMAGE: 'https://images.penguinrandomhouse.com/cover/9780307387899',
+    NUMBER_OF_PAGES: 287,
+    AUTHOR: 'Cormac McCarthy',
+    PRICE: 14
+  },
+
+];
